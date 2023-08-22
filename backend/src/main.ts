731b4f7cb7f33450d9ab34870cf2cf9052203675
DanielRicra <daniel.int@outlook.com>
2023-08-22 10:40:34 -0500

    Setting up  nestjs Swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('FoodieParty')
+    .setDescription(
+      'Whip up a social network for foodies! Users can share their favorite recipes, cooking tips, and food photos. You could even throw in a meal planner and shopping list generator. ',
+    )
+    .setVersion('1.0')
+    .addTag('foodies')
+    .build();
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
