d2a865da6d754e0590bfaf2617623577cfab2523
DanielRicra <daniel.int@outlook.com>
2023-08-23 10:36:35 -0500

    Update main.ts
    - Move swagger config and options to config folder.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,23 +5,15 @@ import {
   SwaggerModule,
 } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { swaggerConfig, swaggerOptions } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('FoodieParty')
-    .setDescription(
-      'Whip up a social network for foodies! Users can share their favorite recipes, cooking tips, and food photos. You could even throw in a meal planner and shopping list generator. ',
-    )
-    .setVersion('1.0')
-    .addTag('foodies')
-    .build();
-  const options: SwaggerDocumentOptions = {
-    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
-  };
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  const document = SwaggerModule.createDocument(app, config, options);
+  const document = SwaggerModule.createDocument(app, swaggerConfig, swaggerOptions);
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
