68f2acca29bc7649c8d6b16cae94848067672edf
DanielRicra <daniel.int@outlook.com>
2023-08-23 15:33:41 -0500

    Update auth service
    Return access token instead of a user data in signIn and signUp

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -4,16 +4,23 @@ import {
   NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
-import { AuthDto, LoginDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto, LoginDto } from './dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
-  async signin(dto: LoginDto) {
+  async signIn(dto: LoginDto): Promise<{ accessToken: string }> {
     try {
       const existingUser = await this.prisma.user.findUniqueOrThrow({
         where: {
@@ -32,7 +39,7 @@ export class AuthService {
 
       delete existingUser.password;
 
-      return existingUser;
+      return this.signToken(existingUser.id, existingUser.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2025') {
@@ -43,20 +50,19 @@ export class AuthService {
     }
   }
 
-  async signup(dto: AuthDto) {
+  async signUp(dto: AuthDto): Promise<{ accessToken: string }> { 
     const hash = await argon.hash(dto.password);
 
     try {
-      const user = await this.prisma.user.create({
+      const newUser = await this.prisma.user.create({
         data: {
           email: dto.email,
           password: hash,
           username: dto.username,
         },
       });
-      delete user.password;
 
-      return user;
+      return this.signToken(newUser.id, newUser.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -68,5 +74,25 @@ export class AuthService {
 
       throw error;
     }
+  }
+
+  async signToken(
+    userId: string,
+    email: string,
+  ): Promise<{ accessToken: string }> {
+    const userForToken = {
+      id: userId,
+      email,
+    };
+
+    const secret = this.config.get('JWT_SECRET');
+
+    const accessToken = await this.jwt.signAsync(userForToken, {
+      expiresIn: '15m',
+      secret,
+    });
+    return {
+      accessToken,
+    };
   }
 }
