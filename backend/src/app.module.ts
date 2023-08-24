3dddff07312f6f18909ec5c69e1199e664bee1a0
DanielRicra <daniel.int@outlook.com>
2023-08-23 21:29:05 -0500

    Add prisma, user, auth and config modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
