866bbb61538dbf22a38c61de54e3f609fb195aa1
DanielRicra <daniel.int@outlook.com>
2023-08-23 21:38:19 -0500

    Split own modules from dependencies imports

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
+
 import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 
